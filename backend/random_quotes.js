69948fe3898f8bb98f2d4788296ce4f8ee78cb42
Ammad <anosidium@hotmail.com>
2026-07-11 19:27:40 +0100

    Display a message for errors.

--- backend/random_quotes.js
+++ backend/random_quotes.js
@@ -2,8 +2,9 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
 const port = 3000;
+
+app.use(cors());
 
 const quotes = [
   {
--- frontend/script.js
+++ frontend/script.js
@@ -5,22 +5,32 @@ async function fetchRandomQuote() {
     const response = await fetch(url);
 
     if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+      const message = `Response status: ${response.status}`;
+      displayMessage(message);
+      throw new Error(message);
     }
 
     const quote = await response.json();
 
     if (!quote) {
-      throw new Error(`Response json: ${quote}`);
+      const message = `Response json: ${quote}`;
+      displayMessage(message);
+      throw new Error(message);
     }
 
     const quoteElement = document.getElementById("quote");
     quoteElement.textContent = `${quote.quote} – ${quote.author}`;
+    displayMessage("");
   } catch (error) {
-    console.log(error.message);
+    displayMessage(error.message);
   }
 }
 
+function displayMessage(message) {
+  messageElement.textContent = `${message}`;
+}
+
+const messageElement = document.getElementById("message");
 const randomQuoteButton = document.getElementById("random-quote-button");
 
 window.onload = () => {
